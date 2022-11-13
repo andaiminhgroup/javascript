6a9c2b20459aaa6a5dd2272254fdd5f1c0cdc3a8
AN ĐẠI MINH GROUP <72484357+andaiminhgroup@users.noreply.github.com>
2022-11-13 15:39:00 +0700

    Create loadJS.js

---
+++ loadJS/loadJS.js
@@ -0,0 +1,18 @@
+        //<![CDATA[
+        //CSS Ready
+        function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+        loadCSS("//fonts.googleapis.com/css?family=Roboto:100,100i,300,400,500,600,700,800i,900&subset=latin-ext,greek-ext,cyrillic-ext,greek,vietnamese,latin,cyrillic");
+        loadCSS("//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=latin-ext,greek-ext,cyrillic-ext,greek,vietnamese,latin,cyrillic");
+        loadCSS("//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=latin-ext,greek-ext,cyrillic-ext,greek,vietnamese,latin,cyrillic");
+        loadCSS("//fonts.googleapis.com/css?family=Playfair+Display:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=latin-ext,greek-ext,cyrillic-ext,greek,vietnamese,latin,cyrillic");
+        loadCSS("//fonts.googleapis.com/css?family=Bitter:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=latin-ext,greek-ext,cyrillic-ext,greek,vietnamese,latin,cyrillic");
+        loadCSS("//fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=latin-ext,greek-ext,cyrillic-ext,greek,vietnamese,latin,cyrillic");
+        loadCSS("//fonts.googleapis.com/css?family=Old+Standard+TT:regular,italic,700&subset=latin,cyrillic-ext,cyrillic,latin-ext,vietnamese");
+        loadCSS("//fonts.googleapis.com/css?family=Lato:300,400,700");
+        loadCSS("//fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;1,400;1,500&display=swap");
+        loadCSS("//fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600&display=swap");
+        loadCSS("//fonts.googleapis.com/css?family=Crimson+Text:400,600,700,400italic");
+        loadCSS("//fonts.googleapis.com/css?family=Niconne");
+        loadCSS("//fonts.googleapis.com/css?family=Titillium+Web:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=latin-ext,greek-ext,cyrillic-ext,greek,vietnamese,latin,cyrillic");
+        loadCSS("//cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css");
+        //]]>
